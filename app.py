6f45b9404ad4a839161103770f9f3f3bfde3dfe1
iqramahamed <iqra.mahamed64@gmail.com>
2024-06-09 21:46:31 +0300

    save

--- app.py
+++ app.py
@@ -8,23 +8,24 @@ class TestModels(unittest.TestCase):
     def setUp(self):
         self.cursor = MagicMock()
 
-    def test_author_creation(self):
-        author = Author(1, "John Doe")
-        self.assertEqual(author.name, "John Doe")
-
     def test_article_creation(self):
         article = Article(1, "Test Title", "Test Content", 1, 1)
         self.assertEqual(article.title, "Test Title")
-
-    def test_magazine_creation(self):
-        magazine = Magazine(1, "Tech Weekly", "Technology")
-        self.assertEqual(magazine.name, "Tech Weekly")
 
     def test_create_author(self):
         author = Author(None, "John Doe")
         author.create_author(self.cursor)
         self.cursor.execute.assert_called_once_with("INSERT INTO authors (name) VALUES (?)", ("John Doe",))
 
+    def test_author_creation(self):
+        author = Author(1, "John Doe")
+        self.assertEqual(author.name, "John Doe")
+
+    def test_magazine_creation(self):
+        magazine = Magazine(1, "Tech Weekly", "Technology")
+        self.assertEqual(magazine.name, "Tech Weekly")
+
+    
     def test_get_all_authors(self):
         self.cursor.fetchall.return_value = [(1, "John Doe"), (2, "John Doe")]
         authors = Author.get_all_authors(self.cursor)
@@ -35,15 +36,7 @@ class TestModels(unittest.TestCase):
         self.assertEqual(authors[1].id, 2)
         self.assertEqual(authors[1].name, "John Doe")
 
-    def test_articles(self):
-        self.cursor.fetchall.return_value = [(1, "Test Article", "Test Content", 1, 1)]
-        author = Author(1, "John Doe")
-        articles = author.articles(self.cursor)
-        self.cursor.execute.assert_called_once_with("SELECT * FROM articles WHERE author_id = ?", (1,))
-        self.assertEqual(len(articles), 1)
-        self.assertEqual(articles[0][0], 1) 
-        self.assertEqual(articles[0][1], "Test Article") 
-
+  
     def test_magazines(self):
         self.cursor.fetchall.return_value = [(1, "Tech Magazine", "Technology")]
         author = Author(1, "John Doe")
@@ -57,6 +50,16 @@ class TestModels(unittest.TestCase):
         self.assertEqual(len(magazines), 1)
         self.assertEqual(magazines[0][0], 1)  
         self.assertEqual(magazines[0][1], "Tech Magazine") 
+
+    def test_articles(self):
+        self.cursor.fetchall.return_value = [(1, "Test Article", "Test Content", 1, 1)]
+        author = Author(1, "John Doe")
+        articles = author.articles(self.cursor)
+        self.cursor.execute.assert_called_once_with("SELECT * FROM articles WHERE author_id = ?", (1,))
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0][0], 1) 
+        self.assertEqual(articles[0][1], "Test Article") 
+
 
 if __name__ == "__main__":
     unittest.main()
